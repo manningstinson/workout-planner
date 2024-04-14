b60c6e5760daaf71e4128c341eccdd1b38601fdc
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-14 22:06:04 +0000

    updated submit, and retrieve

--- routes/retrieve_exercise.py
+++ routes/retrieve_exercise.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/retrieve-exercise')
 def retrieve_exercise_function():
     # Any necessary logic here
     # For example:
-    # fetch_exercises_from_database()
+    # process_form_data()
     return render_template('retrieve-exercise.html')
+
+if __name__ == '__main__':
+    # Specify the desired host and port here
+    app.run(debug=False, host='0.0.0.0', port=8080)
