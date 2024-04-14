324e62ce25420b0d22371da5afee3ebe9f5818bb
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-14 14:21:23 +0000

    updated db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ import mysql.connector
 # Define database connection details using environment variables
 DB_CONFIG = {
     'host': os.getenv('_self.HOSTNAME'),
-    'port': os.getenv('_self.PORT'),
+    'port': os.getenv('_self.PORT'),  # Keep the port as a string
     'user': os.getenv('_self.USERNAME'),
     'password': os.getenv('_self.PASSWORD'),
     'database': os.getenv('_self.DATABASE'),
@@ -15,6 +15,10 @@ DB_CONFIG = {
 def create_connection():
     conn = None
     try:
+        # Check if the port environment variable is set and not None
+        if DB_CONFIG['port'] is not None:
+            # Convert the port to an integer if it's not None
+            DB_CONFIG['port'] = int(DB_CONFIG['port'])
         conn = mysql.connector.connect(**DB_CONFIG)
     except mysql.connector.Error as e:
         print(e)
