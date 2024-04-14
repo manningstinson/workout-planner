1fe988799e960576231ffe73b039929bbcccb1ae
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-14 21:59:39 +0000

    updated submit-exercise

--- routes/submit_exercise.py
+++ routes/submit_exercise.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/submit-exercise')
 def submit_exercise_function():
     # Any necessary logic here
     # For example:
     # process_form_data()
     return render_template('submit-exercise.html')
+
+if __name__ == '__main__':
+    # Specify the desired host and port here
+    app.run(debug=False, host='0.0.0.0', port=8080)
