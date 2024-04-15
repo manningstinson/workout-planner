14db2799ac9668a08449f2436d003aba55e451db
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-15 14:08:47 +0000

    updated db_config.py - resolving port number issue

--- db_config.py
+++ db_config.py
@@ -2,19 +2,15 @@ import os
 import mysql.connector
 
 def create_connection():
-    # Print the value of _self.PORT for debugging
-    port = os.getenv('workout-planner.PORT')
-    print("Port number:", port)
-
     # Define database connection details using environment variables
     DB_CONFIG = {
         'hostname': os.getenv('workout-planner.HOSTNAME'),
-        'port': int(port) if port else None,
+        'port': int(os.getenv('workout-planner.PORT')),  # Ensure port is converted to int
         'username': os.getenv('workout-planner.USERNAME'),
         'password': os.getenv('workout-planner.PASSWORD'),
         'database': os.getenv('workout-planner.DATABASE'),
-        'database_url': os.getenv('workout-planner.DATABASE_URL'),  # Add database URL
-        'ca_cert': os.getenv('workout-planner.CA_CERT'),  # Add CA certificate
+        'database_url': os.getenv('workout-planner.DATABASE_URL'),
+        'ca_cert': os.getenv('workout-planner.CA_CERT'),
     }
 
     conn = None
